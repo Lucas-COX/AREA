6a07fd2a7b85c1e4efb6d0d999c7147df6622ef4
Havok <72014202+Lucas-COX@users.noreply.github.com>
2022-10-03 21:30:53 +0200

    feat(server): send cookie with token on login (#8)

--- server/lib/send_json.go
+++ server/lib/send_json.go
@@ -8,5 +8,16 @@ import (
 func SendJson(w http.ResponseWriter, body any) {
 	bytes, err := json.Marshal(body)
 	CheckError(err)
+	w.WriteHeader(http.StatusOK)
 	w.Write(bytes)
+}
+
+func SetCookie(w http.ResponseWriter, name string, value string) {
+	cookie := &http.Cookie{
+		Name:     name,
+		Value:    value,
+		MaxAge:   0,
+		HttpOnly: true,
+	}
+	http.SetCookie(w, cookie)
 }
