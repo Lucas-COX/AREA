946ad6913d6cd561d6cbd46c404da0ab0dc23213
Morgan <morgan.ehni@epitech.eu>
2022-10-11 10:49:53 +0200

    feat(web): scrollable list of triggers

---
+++ web/components/Card.tsx
@@ -0,0 +1,12 @@
+import { withChildren, withClassName } from "../config/withs";
+
+export default function Card ({ className = "", children }: CardProps) {
+    return (
+        <div className={"rounded-md shadow-md border border-gray-200" + className}>
+            {children}
+        </div>
+    )
+}
+
+export interface CardProps extends withClassName, withChildren {
+}
