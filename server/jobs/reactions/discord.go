54294af982feaf5b474fb71e2c7d70f0a93cca8d
Havok <72014202+Lucas-COX@users.noreply.github.com>
2022-10-12 10:31:24 +0200

    feat(server): send embeds on discord trigger reaction (#16)

--- server/jobs/reactions/discord.go
+++ server/jobs/reactions/discord.go
@@ -6,7 +6,6 @@ import (
 	"Area/lib"
 	"bytes"
 	"encoding/gob"
-	"fmt"
 
 	"github.com/gtuk/discordwebhook"
 )
@@ -15,20 +14,39 @@ func React(reaction models.Reaction, user models.User) {
 	trigger, err := database.Trigger.GetById(reaction.TriggerID, user.ID)
 	var storedData models.TriggerData
 	var buf bytes.Buffer
+	var embeds []discordwebhook.Embed
+	var fields []discordwebhook.Field
 
-	lib.CheckError(err)
+	lib.LogError(err)
 	buf.Write(trigger.Data)
 	err = gob.NewDecoder(&buf).Decode(&storedData)
 
 	lib.LogError(err)
 	var username = "Area"
-	var content = fmt.Sprintf("New Event : %s\t\n %s\t\n %s\t\n", storedData.Author, storedData.Title, storedData.Description)
+	var title = "New event in Area"
+	var color = "1668818"
+	var timestamp = storedData.Timestamp.Format("January 2, 2006") + " at " + storedData.Timestamp.Format("15:04:05")
+	var footer discordwebhook.Footer = discordwebhook.Footer{
+		Text: &timestamp,
+	}
+
 	url := reaction.Token
+	fields = append(fields, discordwebhook.Field{
+		Name:  &storedData.Title,
+		Value: &storedData.Description,
+	})
+	embeds = append(embeds, discordwebhook.Embed{
+		Title:       &title,
+		Description: &storedData.Author,
+		Fields:      &fields,
+		Color:       &color,
+		Footer:      &footer,
+	})
 	switch reaction.Action {
 	case "send":
 		message := discordwebhook.Message{
 			Username: &username,
-			Content:  &content,
+			Embeds:   &embeds,
 		}
 		err := discordwebhook.SendMessage(url, message)
 		lib.LogError(err)
