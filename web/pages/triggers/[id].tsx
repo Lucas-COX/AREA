dfeabc6953d8e382b8ab4ad19bd54e795578a6b6
Havok <72014202+Lucas-COX@users.noreply.github.com>
2022-11-06 16:24:16 +0100

    feat(web): election des triggers dynamiquement avec le serveur (#48)
    * feat(web): now able to create a trigger with NO action nor reaction
    
    * feat(web): select action
    
    * feat(web): use services instead of actions and reactions
    
    Co-authored-by: Morgan <morgan.ehni@epitech.eu>

--- web/pages/triggers/[id].tsx
+++ web/pages/triggers/[id].tsx
@@ -1,27 +1,37 @@
 import type { GetServerSidePropsContext } from 'next';
+import type { Trigger } from '../../config/types';
 import { useRouter } from 'next/router';
 import React, { useState } from 'react';
 import {
-  Paper, TextField, Button, Switch,
+  Paper, TextField, Button, Switch, Select, InputLabel, MenuItem, SelectChangeEvent
 } from '@mui/material';
 import { toast } from 'react-toastify';
 import axios from 'axios';
-import Image from 'next/image';
-import InputAdornment from '@mui/material/InputAdornment';
 import { TrendingFlatOutlined } from '@mui/icons-material';
 import URLSafeBase64 from 'urlsafe-base64';
-import icons from '../../lib/icons';
 import AppLayout from '../../components/AppLayout';
 import { getSession } from '../../lib/session';
 import { withSession } from '../../config/withs';
+import useServices from '../../hooks/useServices';
+import Spinner from '../../components/Spinner';
+
+interface TriggerPageState {
+  trigger: Trigger;
+  actionService?: String;
+  reactionService?: String;
+}
+
 
 export default function TriggerPage({ session }: TriggerProps) {
   const router = useRouter();
   const { id } = router.query;
   const trigger = session?.user?.triggers?.find((t) => t.id === Number(id));
-  const [state, setState] = useState({
-    trigger,
+  const [state, setState] = useState<TriggerPageState>({
+    trigger: trigger as Trigger,
+    actionService: undefined,
+    reactionService: undefined,
   });
+  const {services, setServices, loading, error} = useServices(session.token as string)
 
   if (trigger === undefined) {
     return router.push('/');
@@ -30,6 +40,7 @@ export default function TriggerPage({ session }: TriggerProps) {
   const handleTitleChange = (e: any) => {
     if (state.trigger !== undefined) {
       setState({
+        ...state,
         trigger: {
           ...state.trigger,
           title: e.target.value,
@@ -40,6 +51,7 @@ export default function TriggerPage({ session }: TriggerProps) {
   const handleDescriptionChange = (e: any) => {
     if (state.trigger !== undefined) {
       setState({
+        ...state,
         trigger: {
           ...state.trigger,
           description: e.target.value,
@@ -47,22 +59,23 @@ export default function TriggerPage({ session }: TriggerProps) {
       });
     }
   };
-  const handleReactionTokenChange = (e: any) => {
-    if (state.trigger !== undefined) {
-      setState({
-        trigger: {
-          ...state.trigger,
-          reaction: {
-            ...state.trigger.reaction,
-            token: e.target.value,
-          },
-        },
-      });
-    }
-  };
+  // const handleReactionTokenChange = (e: any) => {
+  //   if (state.trigger !== undefined) {
+  //     setState({
+  //       trigger: {
+  //         ...state.trigger,
+  //         reaction: {
+  //           ...state.trigger.reaction,
+  //           token: e.target.value,
+  //         },
+  //       },
+  //     });
+  //   }
+  // };
   const handleToggle = async (e: React.ChangeEvent<HTMLInputElement>) => {
     if (state.trigger !== undefined) {
       setState({
+        ...state,
         trigger: {
           ...state.trigger,
           active: !state.trigger.active,
@@ -86,22 +99,49 @@ export default function TriggerPage({ session }: TriggerProps) {
       console.error(e);
     }
   };
+
+
   const handleGoogleLogin = async () => {
     try {
-      const location = `http://localhost:3000${router.asPath}`;
-      const url = URLSafeBase64.encode(Buffer.from(location));
+      const url = URLSafeBase64.encode(Buffer.from(`${process.env.NEXT_PUBLIC_API_URL}/login/done`));
 
-      const response = axios.get(`${process.env.NEXT_PUBLIC_API_URL}/providers/google/auth?callback=${url}`, {
+      const response = await axios.get(`${process.env.NEXT_PUBLIC_API_URL}/providers/google/auth?callback=${url}`, {
         headers: { Authorization: `Bearer ${session.token}` },
       });
-      router.push((await response).data.url);
+      if (response.data.url !== null) {
+        window.open(response.data.url, '_blank')?.focus();
+      }
     } catch (e) {
       toast.error('Failed to redirect to Google authentication page.');
     }
   };
 
-  const ActionIcon = <Image src={icons[trigger.action.type]} alt={`${trigger.action.type} icon`} width="15" height="15" />;
-  const ReactionIcon = <Image src={icons[trigger.reaction.type]} alt={`${trigger.reaction.type} icon`} width="15" height="15" />;
+  const handleActionServiceChange = (e: SelectChangeEvent) => {
+    setState({ ...state, actionService: e.target.value })
+  }
+  const handleActionEventChange = (e: SelectChangeEvent) => {
+    setState({ ...state,
+      trigger: {
+        ...state.trigger,
+        action_service: e.target.value,
+      }
+    })
+  }
+
+  if (loading) {
+    return (
+      <AppLayout
+        type="centered"
+        loggedIn
+      >
+        <Spinner />
+      </AppLayout>
+    )
+  }
+
+  const filteredServices = services.filter((service) => session?.user?.services.includes(service.name))
+  const filteredActions = filteredServices.filter((service) => service.name === state.actionService).map((service) => service.actions).flat()
+  console.log(filteredActions)
 
   return (
     <AppLayout
@@ -128,21 +168,29 @@ export default function TriggerPage({ session }: TriggerProps) {
         <div className="h-full flex items-center space-x-6">
           <div className="flex flex-col space-y-4 items-center justify-evenly w-full h-1/2 border rounded-lg bg-primary/5">
             <div>Action</div>
-            <Button
-              variant="outlined"
-              color="primary"
-              startIcon={ActionIcon}
-              className="h-10"
-              disabled={session.user?.google_logged}
-              onClick={handleGoogleLogin}
+            <Select
+              value={state.actionService ? state.actionService.toString() : "undefined" }
+              label="Service"
+              onChange={handleActionServiceChange}
             >
-              {session.user?.google_logged ? 'Logged in' : 'Login with Google'}
-            </Button>
+              {filteredServices.map((service) => (
+                <MenuItem key={`service-pick-${service.name}`} value={service.name}>{service.name.toUpperCase()}</MenuItem>
+              ))}
+            </Select>
+            {state.actionService && state.actionService !== "undefined" && <Select
+              value={state.trigger?.action || (filteredActions[0] && filteredActions[0].name)}
+              label="Event"
+              onChange={handleActionEventChange}
+            >
+              {filteredActions.map((action) => (
+                <MenuItem key={`action-${action.name}`} value={action.name}>{action.name}</MenuItem>
+              ))}
+            </Select>}
           </div>
           <TrendingFlatOutlined fontSize="large" color="secondary" />
           <div className="flex flex-col space-y-4 items-center justify-evenly w-full h-1/2 border rounded-lg bg-secondary/5">
             <div>Reaction</div>
-            <TextField
+            {/* <TextField
               label="Discord webhook url"
               className="bg-white"
               value={state.trigger && state.trigger.reaction.token}
@@ -155,7 +203,7 @@ export default function TriggerPage({ session }: TriggerProps) {
                   </InputAdornment>
                 ),
               }}
-            />
+            /> */}
           </div>
         </div>
         <div className="justify-self-end space-x-4">
