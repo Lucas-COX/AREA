b034da2d414bfbf1c5549661e85023318dcf672c
Lucas Cox <lucas.cox@epitech.eu>
2022-11-07 11:45:59 +0100

    feat(web): add side menu

--- web/components/AppLayout.tsx
+++ web/components/AppLayout.tsx
@@ -1,11 +1,11 @@
 import Head from 'next/head';
 import { Button } from '@mui/material';
 import { toast } from 'react-toastify';
-import axios from 'axios';
 import nookies from 'nookies';
 import { useRouter } from 'next/router';
 import clsx from 'clsx';
 import CenteredLayout from './CenteredLayout';
+import SideMenu from './SideMenu';
 import { withChildren, withClassName } from '../config/withs';
 
 export type LayoutType = 'centered'
@@ -38,7 +38,8 @@ export default function AppLayout({
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className="w-screen h-screen">
-        <div className="w-full h-20 flex justify-end border-b top-0 left-0 border-secondary/30 bg-white shadow-md p-4 absolute">
+        <div className="w-full h-20 flex justify-between items-center border-b top-0 left-0 border-secondary/30 bg-white shadow-md p-4 absolute">
+          <SideMenu />
           {loggedIn && <Button variant="outlined" onClick={handleLogout}>Logout</Button>}
         </div>
         <Layout className={clsx(className, 'pt-20')}>
