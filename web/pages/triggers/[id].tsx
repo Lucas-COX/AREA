d9b2f7adab2ac4570f4c54a971a7a1acf2995849
Lucas Cox <lucas.cox@epitech.eu>
2022-11-09 14:26:44 +0100

    fix(web): action and reaction selection now functional

--- web/pages/triggers/[id].tsx
+++ web/pages/triggers/[id].tsx
@@ -1,5 +1,5 @@
 import type { GetServerSidePropsContext } from 'next';
-import type { Trigger } from '../../config/types';
+import type { Trigger, Service } from '../../config/types';
 import { useRouter } from 'next/router';
 import React, { useState } from 'react';
 import {
@@ -16,8 +16,6 @@ import Spinner from '../../components/Spinner';
 
 interface TriggerPageState {
   trigger: Trigger;
-  actionService: String;
-  reactionService: String;
 }
 
 
@@ -27,8 +25,6 @@ export default function TriggerPage({ session }: TriggerProps) {
   const trigger = session?.user?.triggers?.find((t) => t.id === Number(id));
   const [state, setState] = useState<TriggerPageState>({
     trigger: trigger as Trigger,
-    actionService: String("undefined"),
-    reactionService: String("undefined"),
   });
   const {services, setServices, loading, error} = useServices(session.token as string)
 
@@ -58,19 +54,7 @@ export default function TriggerPage({ session }: TriggerProps) {
       });
     }
   };
-  // const handleReactionTokenChange = (e: any) => {
-  //   if (state.trigger !== undefined) {
-  //     setState({
-  //       trigger: {
-  //         ...state.trigger,
-  //         reaction: {
-  //           ...state.trigger.reaction,
-  //           token: e.target.value,
-  //         },
-  //       },
-  //     });
-  //   }
-  // };
+
   const handleToggle = async (e: React.ChangeEvent<HTMLInputElement>) => {
     if (state.trigger !== undefined) {
       setState({
@@ -84,6 +68,7 @@ export default function TriggerPage({ session }: TriggerProps) {
   };
   const handleApply = async (e: any) => {
     try {
+      console.log(state.trigger);
       await toast.promise(axios.put(`${process.env.NEXT_PUBLIC_API_URL}/triggers/${trigger.id}`, {
         ...state.trigger,
       }, {
@@ -100,13 +85,32 @@ export default function TriggerPage({ session }: TriggerProps) {
   };
 
   const handleActionServiceChange = (e: SelectChangeEvent) => {
-    setState({ ...state, actionService: e.target.value })
+    var action = state.trigger.action
+    if (e.target.value !== "undefined") {
+      action = (services.find((s) => s.name === e.target.value) as Service).actions[0].name;
+    }
+    setState({ ...state, trigger: { ...state.trigger, action_service: e.target.value, action }});
   }
   const handleActionEventChange = (e: SelectChangeEvent) => {
     setState({ ...state,
       trigger: {
         ...state.trigger,
-        action_service: e.target.value,
+        action: e.target.value,
+      }
+    })
+  }
+  const handleReactionServiceChange = (e: SelectChangeEvent) => {
+    var reaction = state.trigger.reaction
+    if (e.target.value !== "undefined") {
+      reaction = (services.find((s) => s.name === e.target.value) as Service).reactions[0].name;
+    }
+    setState({ ...state, trigger: { ...state.trigger, reaction_service: e.target.value, reaction }})
+  }
+  const handleReactionActionChange = (e: SelectChangeEvent) => {
+    setState({ ...state,
+      trigger: {
+        ...state.trigger,
+        reaction: e.target.value,
       }
     })
   }
@@ -124,8 +128,8 @@ export default function TriggerPage({ session }: TriggerProps) {
 
   const filteredActionServices = services.filter((service) => session?.user?.services.includes(service.name) && service.actions.length !== 0)
   const filteredReactionServices = services.filter((service) => session?.user?.services.includes(service.name) && service.reactions.length !== 0)
-  const filteredActions = filteredActionServices.filter((service) => service.name === state.actionService).map((service) => service.actions).flat()
-  const filteredReactions = filteredReactionServices.filter((service) => service.name === state.reactionService).map((service) => service.reactions).flat()
+  const filteredActions = filteredActionServices.filter((service) => service.name === state.trigger.action_service).map((service) => service.actions).flat()
+  const filteredReactions = filteredReactionServices.filter((service) => service.name === state.trigger.reaction_service).map((service) => service.reactions).flat()
   filteredActionServices.push({ name: 'undefined', actions: [], reactions: [] })
   filteredReactionServices.push({ name: 'undefined', actions: [], reactions: [] })
 
@@ -135,7 +139,7 @@ export default function TriggerPage({ session }: TriggerProps) {
       className="flex flex-col items-center justify-center bg-blue-50/50"
       loggedIn
     >
-      <Paper className="w-2/3 h-2/3 flex flex-col justify-between p-10">
+      <Paper className="w-full h-full  sm:w-4/5 sm:h-4/5 md:w-3/4 md:h-3/4 flex flex-col justify-between p-10">
         <div className="flex flex-col space-y-6">
           <TextField
             label="Title"
@@ -154,42 +158,50 @@ export default function TriggerPage({ session }: TriggerProps) {
         <div className="h-full flex items-center space-x-6">
           <div className="flex flex-col space-y-4 items-center justify-evenly w-full h-1/2 border rounded-lg bg-primary/5">
             <div>Action</div>
-            <Select
-              value={state.actionService ? state.actionService.toString() : "undefined"}
-              label="Service"
-              onChange={handleActionServiceChange}
-            >
-              {filteredActionServices.map((service) => (
-                <MenuItem key={`service-pick-${service.name}`} value={service.name}>{service.name.toUpperCase()}</MenuItem>
+            <div className="flex w-full justify-evenly">
+              <Select
+                value={state.trigger.action_service ? state.trigger.action_service.toString() : "undefined"}
+                label="Service"
+                onChange={handleActionServiceChange}
+              >
+                {filteredActionServices.map((service) => (
+                  <MenuItem key={`service-pick-${service.name}`} value={service.name}>{service.name.toUpperCase()}</MenuItem>
+                ))}
+              </Select>
+              {filteredActions.length !== 0 && <Select
+                value={state.trigger?.action || (filteredActions[0] && filteredActions[0].name)}
+                label="Action"
+                onChange={handleActionEventChange}
+              >
+              {filteredActions.map((action) => (
+                  <MenuItem key={`action-${action.name}`} value={action.name}>{action.name}</MenuItem>
               ))}
-            </Select>
-            {filteredActions.length !== 0 && <Select
-              value={state.trigger?.action || (filteredActions[0] && filteredActions[0].name)}
-              label="Event"
-              onChange={handleActionEventChange}
-            >
-            {filteredActions.map((action) => (
-                <MenuItem key={`action-${action.name}`} value={action.name}>{action.name}</MenuItem>
-            ))}
-            </Select>}
+              </Select>}
+            </div>
           </div>
           <TrendingFlatOutlined fontSize="large" color="secondary" />
           <div className="flex flex-col space-y-4 items-center justify-evenly w-full h-1/2 border rounded-lg bg-secondary/5">
             <div>Reaction</div>
-            {/* <TextField
-              label="Discord webhook url"
-              className="bg-white"
-              value={state.trigger && state.trigger.reaction.token}
-              onChange={handleReactionTokenChange}
-              InputProps={{
-                className: 'h-10',
-                startAdornment: (
-                  <InputAdornment position="start">
-                    {ReactionIcon}
-                  </InputAdornment>
-                ),
-              }}
-            /> */}
+            <div className="flex w-full justify-evenly">
+              <Select
+                value={state.trigger.reaction_service ? state.trigger.reaction_service.toString() : "undefined"}
+                label="Service"
+                onChange={handleReactionServiceChange}
+              >
+                {filteredReactionServices.map((service) => (
+                  <MenuItem key={`service-pick-${service.name}`} value={service.name}>{service.name.toUpperCase()}</MenuItem>
+                ))}
+              </Select>
+              {filteredReactions.length !== 0 && <Select
+                value={state.trigger?.reaction || (filteredReactions[0] && filteredReactions[0].name)}
+                label="Action"
+                onChange={handleReactionActionChange}
+              >
+              {filteredReactions.map((reaction) => (
+                  <MenuItem key={`action-${reaction.name}`} value={reaction.name}>{reaction.name}</MenuItem>
+              ))}
+              </Select>}
+            </div>
           </div>
         </div>
         <div className="justify-self-end space-x-4">
