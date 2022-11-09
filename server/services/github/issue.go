39574209b8c4739226e5a1954c1579cc5da6fdba
Havok <72014202+Lucas-COX@users.noreply.github.com>
2022-11-09 16:18:21 +0100

    feat(server): implement github issue closed action (#69)

--- server/services/github/issue.go
+++ server/services/github/issue.go
@@ -30,6 +30,24 @@ func compareIssueData(issue issue, trigger *models.Trigger) bool {
 	return false
 }
 
+func compareClosedIssueData(issue closedIssue, trigger *models.Trigger) bool {
+	var newData models.TriggerData
+	var oldData models.TriggerData
+	gob.NewDecoder(bytes.NewReader(trigger.Data)).Decode(&oldData)
+	issueTimestamp, _ := time.Parse(time.RFC3339, issue.ClosedAt)
+	if oldData.Timestamp.Before(issueTimestamp) {
+		newData = oldData
+		newData.Timestamp = issueTimestamp.Local()
+		newData.Title = issue.Title
+		newData.Author = "Was opened by " + issue.Author.Login + " in " + issue.Repository.Name
+		newData.Description = issue.Body
+		trigger.Data = lib.EncodeToBytes(newData)
+		database.Trigger.Update(trigger)
+		return true
+	}
+	return false
+}
+
 func checkNewIssue(srv *githubv4.Client, triggerId uint, userId uint) bool {
 	var query issuesQuery
 	var oldData models.TriggerData
@@ -62,4 +80,26 @@ func createIssue(srv *githubv4.Client, storedData models.TriggerData, action str
 	}
 	err := srv.Mutate(context.Background(), &mutation, input, nil)
 	lib.LogError(err)
+}
+
+func checkClosedIssue(srv *githubv4.Client, trggerId uint, userId uint) bool {
+	var query closedIssuesQuery
+	var oldData models.TriggerData
+	trigger, _ := database.Trigger.GetById(trggerId, userId)
+	gob.NewDecoder(bytes.NewReader(trigger.Data)).Decode(&oldData)
+
+	user := getUser(srv)
+	err := srv.Query(context.Background(), &query, map[string]interface{}{
+		"owner": githubv4.String(user.Viewer.Login),
+		"name":  githubv4.String(oldData.ActionData),
+	})
+	if err != nil {
+		lib.LogError(err)
+		return false
+	}
+	gob.NewDecoder(bytes.NewReader(trigger.Data)).Decode(&oldData)
+	if len(query.Repository.Issues.Edges) == 0 {
+		return false
+	}
+	return compareClosedIssueData(query.Repository.Issues.Edges[0].Node, trigger)
 }
